ee31062afff81be19c41318b5a533ce0349173ce
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-04-05 22:11:47 -0300

    strings

---
+++ 08_strings.rb
@@ -0,0 +1,28 @@
+x = "Jackson"
+y = 'Rails'
+
+puts x
+puts x
+puts y.class
+puts y.class
+
+a = "Curso"
+b = "Rails"
+
+puts a << b # modifica o a
+puts a + b
+
+x = "curso"
+puts x.object_id
+x = x + "rails"
+puts x
+puts x.object_id
+###############
+q = "curso de "
+puts q.object_id
+q << "rails"
+puts q
+puts q.object_id
+
+h = "Jackson #{1+1} Pires #{q}"
+puts h
