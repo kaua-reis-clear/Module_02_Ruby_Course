77191d92bca6db6c68506feaecd3e92325dc9d2c
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-04-05 21:14:46 -0300

    arrays

---
+++ 06_array.rb
@@ -0,0 +1,27 @@
+v = [[11,12,13], [21,22,23], [31, 32, 33]]
+
+v.each do |externo|
+    externo.each do |interno|
+        puts interno
+    end
+end
+
+puts '-----------------------'
+
+s = 'Jackson'
+
+puts s[0]
+
+puts '-----------------------'
+
+v = [1,3,5,6,7,9]
+
+v.each do |elem|
+    puts elem
+end
+
+v1 = [] # Array.new
+v1.push(4)
+v1.push('Jack')
+v1.push('Hello')
+v1.push(7)
