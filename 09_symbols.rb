7b9bde1413d183d05e3adfda5b04c6647ae99a69
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-04-05 22:16:13 -0300

    symbols

---
+++ 09_symbols.rb
@@ -0,0 +1,11 @@
+puts "Jackson".object_id
+puts "Jackson".object_id
+puts "Jackson".object_id
+puts "Jackson".object_id
+puts "Jackson".object_id
+puts "================"
+puts :jackson.object_id
+puts :jackson.object_id
+puts :jackson.object_id
+puts :jackson.object_id
+puts :jackson.object_id
